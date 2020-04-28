be271b7613e51641a014c5701f4fc61235c52cfa
Manjunathshetty <64469369+Manjunathshetty@users.noreply.github.com>
2020-04-28 18:22:33 +0530

    Add files via upload

---
+++ test3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,j;
+    cout<<"enter the value of a";;
+    cin>>a;
+    for(j=1; j<= 2*a; j++)
+	{
+		if(j%2 != 0)
+		{
+			cout<< j <<" , ";
+while(a%2!=0)
+{
+	cout<<j<<",";
+}
+		} }
+	return 0;
+}
